c99fde16bd1ab37866e2daa1dc6fcf9497ba0031
kosenin <kosenin@yandex.ru>
2019-11-21 15:07:46 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static int squirrel(int N) {
+        int fact = 1;
+        for (int i = 2; i <= N; i++) {
+            fact = fact * i;
+        }
+        char[] smaragd = String.valueOf(fact).toCharArray();
+
+        return Integer.parseInt(String.valueOf(smaragd[0]));
+    }
+}
