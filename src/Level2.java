5bf1c4c35a645e3a9072f95cc4abefa08e5382ba
kosenin <kosenin@yandex.ru>
2019-11-21 16:38:10 +0100

    Level 2

---
+++ src/Level2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Level2 {
+
+
+    public static int odometer(int[] oksana) {
+
+        List<Integer> speed = new ArrayList();
+        List<Integer> hours = new ArrayList();
+
+        for (int i = 0; i < oksana.length; i++) {
+            if (i % 2 == 0) {
+                speed.add(oksana[i]);
+            } else
+                hours.add(oksana[i]);
+        }
+
+        int realTIme = 0;
+
+        int counter = 0;
+
+        for (int i = 0; i < speed.size(); i++) {
+            if (i == 0) {
+                counter = counter + ((speed.get(i) * hours.get(i)));
+            } else {
+                counter = counter + (speed.get(i) * (hours.get(i) - hours.get(realTIme)));
+                realTIme++;
+            }
+        }
+
+        return counter;
+    }
+}
